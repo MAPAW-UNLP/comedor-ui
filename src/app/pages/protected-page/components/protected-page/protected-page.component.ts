565d1d78cb2ccf4f676c1c2e56ba479945e3faed
Patricio Ezequiel Hondagneu Roig <5291103+patricio-ezequiel-hondagneu-roig@users.noreply.github.com>
2020-09-25 16:03:08 -0300

    Add protected page to test the authentication restrictions.

---
+++ src/app/pages/protected-page/components/protected-page/protected-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+/**
+ * Top-level component of the ProtectedPage module.
+ */
+@Component({
+	selector: 'cu-protected-page',
+	templateUrl: './protected-page.component.html',
+	styleUrls: [ './protected-page.component.scss' ],
+})
+export class ProtectedPageComponent {
+
+}
--- src/app/pages/root/root-routing.module.ts
+++ src/app/pages/root/root-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from 'src/app/auth/guards/auth/auth.guard';
 import { NotFoundPageComponent } from '../not-found-page/components/not-found-page/not-found-page.component';
 import { NotFoundPageModule } from '../not-found-page/not-found-page.module';
+import { ProtectedPageComponent } from '../protected-page/components/protected-page/protected-page.component';
+import { ProtectedPageModule } from '../protected-page/protected-page.module';
 import { PublicPageComponent } from '../public-page/components/public-page/public-page.component';
 import { PublicPageModule } from '../public-page/public-page.module';
 
@@ -10,6 +13,13 @@ const routes: Routes = [
 		path: '',
 		pathMatch: 'full',
 		component: PublicPageComponent,
+	},
+	{
+		path: 'protected-page',
+		component: ProtectedPageComponent,
+		canActivate: [
+			AuthGuard,
+		],
 	},
 	{
 		path: '404',
@@ -27,8 +37,9 @@ const routes: Routes = [
 @NgModule({
 	imports: [
 		RouterModule.forRoot( routes ),
-		NotFoundPageModule,
 		PublicPageModule,
+		ProtectedPageModule,
+		NotFoundPageModule,
 	],
 	exports: [
 		RouterModule,
