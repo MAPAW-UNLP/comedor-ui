12ab6bd989802c1e885621756736a2d15f0e25b5
Andres Rodriguez <andrurodr10@gmail.com>
2020-10-20 11:58:56 -0300

    checkout page

--- src/app/pages/shopping-cart-page/components/shopping-cart-page/shopping-cart-page.component.ts
+++ src/app/pages/shopping-cart-page/components/shopping-cart-page/shopping-cart-page.component.ts
@@ -1,7 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import moment from 'moment';
+import { Menu } from 'src/app/models/menu.model';
 import { CartService } from 'src/app/shared/services/cart/cart.service';
+import { creditCardExpirancyValidator, creditCardNumberValidator } from 'src/app/shared/validators/credit-card.validator';
 
+
+interface PaymentMethod {
+	id: string;
+	name: string;
+}
 /**
  * Top-level component of the ShoppingCartPage module.
  */
@@ -12,8 +20,17 @@ import { CartService } from 'src/app/shared/services/cart/cart.service';
 })
 export class ShoppingCartPageComponent implements OnInit {
 
-	public firstFormGroup!: FormGroup;
+	public showStepper = false;
+	public paymentMethodFormGroup!: FormGroup;
 	public cardInfoFormGroup!: FormGroup;
+	public cartItems: Menu[] = [];
+
+	public paymentMethods: PaymentMethod[] = [
+		{
+			id: 'CARD',
+			name: 'Tarjeta'
+		}
+	];
 
 	public constructor(
 		private readonly _formBuilder: FormBuilder,
@@ -21,14 +38,58 @@ export class ShoppingCartPageComponent implements OnInit {
 		) { }
 
 	public ngOnInit() {
-		this.firstFormGroup = this._formBuilder.group({
-			firstCtrl: ['', Validators.required]
-		});
+
 		this.cardInfoFormGroup = this._formBuilder.group({
 			name: ['', Validators.required],
-			cardNumber: ['', Validators.required],
-			expirancy: ['', Validators.required],
-			ccv: ['', Validators.required]
+			cardNumber: ['', [Validators.required, creditCardNumberValidator]],
+			expirancy: ['', [Validators.required, creditCardExpirancyValidator]],
+			ccv: ['', [Validators.required]]
 		});
+		this.paymentMethodFormGroup = this._formBuilder.group({
+			paymentMethod: ['', Validators.required]
+		});
+		this.cartItems = this.cartService.getCartItems();
+	}
+
+	public proceedToPayment(): void {
+		if (this.paymentMethodFormGroup.invalid) {
+			return;
+		}
+		this.showStepper = true;
+	}
+
+	public getSummaryOptionText(menu: Menu): string {
+		const date = moment(menu.date, ['DD/MM/YYYY', 'YYYY-MM-DD']).locale('es').format('dddd DD/MM/YYYY');
+		return `${menu.name} - ${date} - ${menu.kitchenSite.name}`;
+	}
+
+	public returnToSummary(): void {
+		this.showStepper = false;
+	}
+
+	public getTotalAmount(): string {
+		return this.cartItems.reduce((acc, current) => acc + current.unitPrice, 0).toString();
+	}
+
+	public removeFromCart(menu: Menu): void {
+		this.cartItems = this.cartService.remove(menu.id);
+	}
+
+
+	public get expirancyDigitCountToExpected( ): string {
+		return `${ this.cardInfoFormGroup.get('expirancy')?.value.length } / 4`;
+	}
+
+	public get ccvDigitCountToExpected( ): string {
+		return `${ this.cardInfoFormGroup.get('ccv')?.value.length } / 3`;
+	}
+
+	public getCardLastNumbers(): string {
+		const cardNumber = this.cardInfoFormGroup.get('cardNumber')?.value.split('');
+		return cardNumber.slice(cardNumber.length - 4, cardNumber.length ).join('');
+	}
+
+	public buy(): void {
+
 	}
 }
---
+++ src/app/shared/validators/credit-card.validator.ts
@@ -0,0 +1,59 @@
+import { ValidatorFn } from '@angular/forms';
+import moment from 'moment';
+
+/**
+ * Function that receives a control and returns a validation error if its value doesn't match that of a DNI,
+ * and _null_ otherwise.
+ */
+
+const creditCardRegex = {
+	visa: /^(?:4[0-9]{12}(?:[0-9]{3})?)$/,
+	mastercard: /^(?:5[1-5][0-9]{14})$/,
+	amex: /^(?:3[47][0-9]{13})$/,
+};
+
+export const creditCardNumberValidator: ValidatorFn = ( control ) => {
+	const cardNumber: string = control.value.trim( );
+	if (cardNumber === '') {
+		return null;
+	} else {
+		const isValid = Object.values(creditCardRegex).some((regex: RegExp) => regex.test(cardNumber));
+		return isValid ? null : { 'invalidCardNumberFormat' : true };
+	}
+};
+
+export const creditCardExpirancyValidator: ValidatorFn = ( control ) => {
+	let expirancy: string;
+	let expirancyMonth: number;
+	let expirancyYear: number;
+	try {
+		expirancy = control.value.trim( );
+		expirancyMonth = Number.parseInt(expirancy.slice(0, 2));
+		expirancyYear = Number.parseInt(expirancy.slice(2, 4));
+	} catch {
+		expirancy = '';
+		expirancyMonth = 0;
+		expirancyYear = 0;
+	}
+	if (expirancy === '') {
+		return null;
+	}
+	if (expirancyMonth > 12 ){
+		return { 'invalidCardExpirancyFormat' : true };
+	}
+	if ( expirancyMonth && expirancyYear ) {
+		const today = moment();
+		const month = today.month() + 1;
+		// Get last 2 digits
+		const year = Number.parseInt(today.format('YY'));
+		let isValid;
+		if (year === expirancyYear) {
+			isValid = expirancyMonth >= month;
+		} else {
+			isValid = expirancyYear >= year;
+		}
+		return isValid ? null : { 'invalidCardExpirancy' : true };
+	}else {
+		return { 'invalidCardExpirancyFormat' : true };
+	}
+};
